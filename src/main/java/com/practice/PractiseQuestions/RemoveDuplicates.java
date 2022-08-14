864128603cd02ae0acec28f827655cc459cfe73d
mariasher001 <mariasher001@gmail.com>
2022-08-14 11:28:39 +0200

    Updated remove duplicates with a runtime of 1ms.

--- src/main/java/com/practice/PractiseQuestions/RemoveDuplicates.java
+++ src/main/java/com/practice/PractiseQuestions/RemoveDuplicates.java
@@ -19,15 +19,13 @@ public class RemoveDuplicates {
         int j =1;
 
         while(j < nums.length){
-            if(nums[i] == nums[j]){
-                j++;
-            } else if (nums[i]!= nums[j]) {
+            if(nums[i] != nums[j]){
                 nums[i+1] =  nums[j];
                 i++;
-                j++;
             }
+            j++;
         }
-        System.out.println(i+1);
+
         return i+1;
     }
 }
