83e115a18ae4a7666be1e64bf69b9e190f938633
mariasher001 <mariasher001@gmail.com>
2022-08-17 09:26:58 +0200

    2 ways to rotate an Array.

--- src/main/java/com/practice/PractiseQuestions/RemoveDuplicates.java
+++ src/main/java/com/practice/PractiseQuestions/RemoveDuplicates.java
@@ -16,16 +16,80 @@ public class RemoveDuplicates {
 
     public static int removeDuplicates(int[] nums) {
         int i = 0;
-        int j =1;
+        int j = 1;
 
-        while(j < nums.length){
-            if(nums[i] != nums[j]){
-                nums[i+1] =  nums[j];
+        while (j < nums.length) {
+            if (nums[i] != nums[j]) {
+                nums[i + 1] = nums[j];
                 i++;
             }
             j++;
         }
 
-        return i+1;
+        return i + 1;
     }
+
+    /**
+     * Given an integer array nums sorted in non-decreasing order,
+     * remove some duplicates in-place such that each unique element appears at most twice.
+     * The relative order of the elements should be kept the same.
+     */
+    public static int removeDuplicatesAppearingMoreThanTwice(int[] nums) {
+        int i = 0;
+        int j = 1;
+        int count = 0;
+        while (j < nums.length) {
+            if (nums[i] == nums[j]) {
+                count++;
+                j++;
+            } else {
+                if (count > 2) {
+                    nums[i + 1] = nums[j];
+                    i++;
+                    count = 0;
+                }
+            }
+
+        }
+        System.out.println(i + 1);
+        return i + 1;
+    }
+
+    public static int removeDuplicates1(int[] nums) {
+        int i = 0;
+        int j = 1;
+        int count = 1;
+        while (j < nums.length) {
+            if (nums[i] == nums[j]) {
+                j++;
+                count++;
+            } else {
+                if (count == 1) {
+                    j++;
+                    i++;
+                } else if (count == 2) {
+                    nums[i + 2] = nums[j];
+                    j++;
+                    i += 2;
+                } else {
+                    nums[i + 2] = nums[j];
+                    j++;
+                    i += 2;
+                }
+                count = 1;
+            }
+        }
+        if(count < 2) {
+            i+=count;
+        } else {
+            i+=2;
+        }
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
+        System.out.println("");
+        System.out.println(i);
+        return i;
+    }
+
 }
---
+++ src/main/java/com/practice/PractiseQuestions/RotateTheArray.java
@@ -0,0 +1,59 @@
+package com.practice.PractiseQuestions;
+
+import java.util.Arrays;
+
+public class RotateTheArray {
+    public static void rotate1(int[] nums, int k) {
+        /**
+         * Solution:
+         * Save the last element
+         * For loop for reversing the array
+         * Add the last element at array[0]
+         * Not a O(1) solution.
+         */
+
+        int count, lastElement = -1;
+        for (count = nums.length - k - 1; count < nums.length - 1; count++) {
+            lastElement = nums[nums.length - 1];
+            for (int i = nums.length - 1; i > 0; i--) {
+                nums[i] = nums[i - 1];
+            }
+            nums[0] = lastElement;
+
+        }
+        //Displaying the loop.
+
+        for (int i = 0; i < nums.length; i++) {
+            System.out.print(nums[i] + " ");
+        }
+    }
+
+    public static void rotateWithConstantSpaceComplexity(int[] nums, int k) {
+        //split from where you want to reverse
+        //Reverse both sub-arrays
+        // reverse together
+        k = k % nums.length;
+        reverseArray( 0,nums.length-1-k, nums);
+        reverseArray(nums.length-1-k, nums.length-1, nums);
+        reverseArray(0, nums.length - 1, nums);
+
+        for (int i = 0; i < nums.length; i++) {
+            System.out.print(nums[i] + " ");
+        }
+
+    }
+
+    private static void reverseArray(int start, int end, int[] nums) {
+        int temp = 0;
+        while( start < end) {
+            temp = nums[start];
+            nums[start] = nums[end];
+            nums[start]=temp;
+            start++;
+            end--;
+
+
+        }
+    }
+
+}
