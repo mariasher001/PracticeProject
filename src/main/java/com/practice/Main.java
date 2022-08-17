c8d16cb0c6ab0e14a1779078ca7c224c29067e28
mariasher001 <mariasher001@gmail.com>
2022-08-17 10:37:21 +0200

    Revert "2 ways to rotate an Array."
    This reverts commit 83e115a18ae4a7666be1e64bf69b9e190f938633.

--- src/main/java/com/practice/Main.java
+++ src/main/java/com/practice/Main.java
@@ -27,7 +27,9 @@ public class Main {
     //SimplificationOfFractions.simplifyFractions(22,11);
         //RomanToInteger.romanToInt("XIX");
        //System.out.println(ValidPalindromePhrase.isPalindrome("hello world"));
-        int[] nums = {1,2,3,4,5,6} ;
-        RotateTheArray.rotateWithConstantSpaceComplexity(nums,2);
+        int[] nums = {1,2,2,3,3,3,3,3,4,5} ;
+        RemoveDuplicates.removeDuplicates(nums);
+        System.out.println();
+
     }
 }
